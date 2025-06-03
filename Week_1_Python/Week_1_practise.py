340515fbedac7aff6d78d24bc7669cc4dd59776d
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-03 20:37:46 +0530

    taking inputs in one line using map(int,input).split()) command

--- Week_1_Python/Week_1_practise.py
+++ Week_1_Python/Week_1_practise.py
@@ -8,3 +8,10 @@ print("the product of ",x, "and ",y ,"are = " ,x*y)
 print("the quotient of ",x, "and ",y ,"are = " ,x/y)
 print("the remainder of ",x, "and ",y ,"are = " ,x%y)
 
+### taking input in same line ###
+x,y=map(int,input("Enter two number with a space between them :").split())    # here we assign two values to two varibles in one line , map(int,input().split()) here split func seprate the string fromwhite space into two strings and the map fnc converts the two strings into integerss
+print("the sum of ",x, "and ",y ,"are = " ,x+y)
+print("the differece of ",x, "and ",y ,"are = " ,x-y)
+print("the product of ",x, "and ",y ,"are = " ,x*y)
+print("the quotient of ",x, "and ",y ,"are = " ,x/y)
+print("the remainder of ",x, "and ",y ,"are = " ,x%y)
