3c782939960cfcd64dd88cfc2f9c5a05c4337c89
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-03 20:59:42 +0530

    done a program for printing area ad perimeter of a square by taking input

--- Week_1_Python/day_1_practise.py
+++ Week_1_Python/day_1_practise.py
@@ -1,17 +1,28 @@
-######   Write a Program to input 2 numbers & print their sum, difference, product, quotient, and remainder   #######
-###     taking input in diffferent lines ###
-x=int(input("Enter one number :"))
-y=int(input("Enter  2nd number :"))
-print("the sum of ",x, "and ",y ,"are = " ,x+y)
-print("the differece of ",x, "and ",y ,"are = " ,x-y)
-print("the product of ",x, "and ",y ,"are = " ,x*y)
-print("the quotient of ",x, "and ",y ,"are = " ,x/y)
-print("the remainder of ",x, "and ",y ,"are = " ,x%y)
+# ######   Write a Program to input 2 numbers & print their sum, difference, product, quotient, and remainder   #######
+# ###     taking input in diffferent lines ###
 
-### taking input in same line ###
-x,y=map(int,input("Enter two number with a space between them :").split())    # here we assign two values to two varibles in one line , map(int,input().split()) here split func seprate the string fromwhite space into two strings and the map fnc converts the two strings into integerss
-print("the sum of ",x, "and ",y ,"are = " ,x+y)
-print("the differece of ",x, "and ",y ,"are = " ,x-y)
-print("the product of ",x, "and ",y ,"are = " ,x*y)
-print("the quotient of ",x, "and ",y ,"are = " ,x/y)
-print("the remainder of ",x, "and ",y ,"are = " ,x%y)
+
+# x=int(input("Enter one number :"))
+# y=int(input("Enter  2nd number :"))
+# print("the sum of ",x, "and ",y ,"are = " ,x+y)
+# print("the differece of ",x, "and ",y ,"are = " ,x-y)
+# print("the product of ",x, "and ",y ,"are = " ,x*y)
+# print("the quotient of ",x, "and ",y ,"are = " ,x/y)
+# print("the remainder of ",x, "and ",y ,"are = " ,x%y)
+
+
+#  ### taking input in same line ###
+
+# x,y=map(int,input("Enter two number with a space between them :").split())    # here we assign two values to two varibles in one line , map(int,input().split()) here split func seprate the string fromwhite space into two strings and the map fnc converts the two strings into integerss
+# print("the sum of ",x, "and ",y ,"are = " ,x+y)
+# print("the differece of ",x, "and ",y ,"are = " ,x-y)
+# print("the product of ",x, "and ",y ,"are = " ,x*y)
+# print("the quotient of ",x, "and ",y ,"are = " ,x/y)
+# print("the remainder of ",x, "and ",y ,"are = " ,x%y)
+
+
+#   ### WAP to input side of a square and print its perimeter and area ###
+
+side=int(input("Enter the integer value of a side of a square:"))
+print("The perimeter and the area of the square of side ",side,"are ",4*side,"and ",side**2," respectively")
+
