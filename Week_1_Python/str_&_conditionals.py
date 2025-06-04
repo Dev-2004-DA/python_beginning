bad3fc2bf831175235b710f4bcc71cba69214821
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-04 12:44:22 +0530

    using multiline string

--- Week_1_Python/str_&_conditionals.py
+++ Week_1_Python/str_&_conditionals.py
@@ -1,6 +1,19 @@
 print("hello")
 print('hello')
-#print("hello "dev"")
+#print("hello "dev"")                   # this will give error bcz the quostes inside and outside the string are same
 print("hello 'dev ' ")
 print('hello "dev" ')
-#print('hello 'dev'')
+#print('hello 'dev'')                  # this will give error bcz the quostes inside and outside the string are same
+
+
+                         ########## Assign string to variable #########
+x='my name \nis dev'                                ##using escape sequence
+y="his name is "
+z="""hlelo sjkhsh """                                           
+w="""hello , my name is dev .                           
+how are you all , i thoought you all are good ,
+nice to see you all """                                             # using triple qutes to print multiline string
+print(x)
+print(y)
+print(z)
+print(w)
