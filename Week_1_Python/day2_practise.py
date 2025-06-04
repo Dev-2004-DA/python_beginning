7eda8f5a3f2435483b61e500fa930eacafc9d919
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-04 16:39:57 +0530

    WAP TO FIND THE OCCURENCE OF $ IN A STRING

--- Week_1_Python/day2_practise.py
+++ Week_1_Python/day2_practise.py
@@ -1,3 +1,9 @@
 ############## WAP TO INPUT USER'S FIRST NAME AND PRINT ITS LENGTH ###############
-x=input("Enter your first name: ")
-print("The length is = " , len(x))
+# x=input("Enter your first name: ")
+# print("The length is = " , len(x))
+
+
+############## WAP TO FIND THE OCCURENCE OF $ IN A STRING ###############
+
+x=input("Enter any string: ")
+print("the occurence the dollar symbol in this string is : ",x.count('$'))
