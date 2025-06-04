afb0398556926920c26693947bbfb6b58df8f4e8
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-04 16:17:43 +0530

    some operation o strings

--- Week_1_Python/str_&_conditionals.py
+++ Week_1_Python/str_&_conditionals.py
@@ -1,19 +1,39 @@
-print("hello")
-print('hello')
-#print("hello "dev"")                   # this will give error bcz the quostes inside and outside the string are same
-print("hello 'dev ' ")
-print('hello "dev" ')
-#print('hello 'dev'')                  # this will give error bcz the quostes inside and outside the string are same
+# print("hello")
+# print('hello')
+# #print("hello "dev"")                   # this will give error bcz the quostes inside and outside the string are same
+# print("hello 'dev ' ")
+# print('hello "dev" ')
+# #print('hello 'dev'')                  # this will give error bcz the quostes inside and outside the string are same
 
 
-                         ########## Assign string to variable #########
-x='my name \nis dev'                                ##using escape sequence
-y="his name is "
-z="""hlelo sjkhsh """                                           
-w="""hello , my name is dev .                           
-how are you all , i thoought you all are good ,
-nice to see you all """                                             # using triple qutes to print multiline string
-print(x)
-print(y)
-print(z)
-print(w)
+#                          ########## Assign string to variable #########
+# x='my name \nis dev'                                ##using escape sequence
+# y="his name is "
+# z="""hlelo sjkhsh """                                           
+# w="""hello , my name is dev .                           
+# how are you all , i thoought you all are good ,
+# nice to see you all """                                             # using triple qutes to print multiline string
+# print(x)
+# print(y)
+# print(z)
+# print(w)
+
+
+########################### BASIC OPERTAIONS ON STRING ######################
+########### CONCATEATION
+x="hello dev"
+y='sagar soni'
+z=""" good
+morning """
+print(x+y+z)
+print(x+" "+y+z)
+############ LENGTH OF STRING 
+print(len(x))
+############ INDEXING
+print(x[0])
+############SLICING OF STRING
+x="Hello World"
+print(x[:3])
+print(x[1:3])
+print(x[3:])
+print(x[-5:-1])
