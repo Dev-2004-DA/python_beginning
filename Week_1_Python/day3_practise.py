a12a0b3bcbf36016245ad9cd3fd1cb98a1fa8f60
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-05 18:36:53 +0530

    WAP to count the number of students with the store the above values in a list ad sort them from ascending to descending

--- Week_1_Python/day3_practise.py
+++ Week_1_Python/day3_practise.py
@@ -21,10 +21,19 @@
 # print("user's favourite movies are = ", User_fav_movies)
 
 ############################## WAP to check if a list contains a palindrome of elements ################
-x=[1,2,3,2,1]
-y=x.copy()
-y.reverse()
-if x==y :
-    print("It is palindrome")
-else:
-    print("It is not ")
+# x=[1,2,3,2,1]
+# y=x.copy()
+# y.reverse()
+# if x==y :
+#     print("It is palindrome")
+# else:
+#     print("It is not ")
+
+############################## WAP to count the number of students with the "A" grade in the following tupple ("C","D","A","A","B","B","A") #########################
+x=("C","D","A","A","B","B","A")
+##store the above values in a list ad sort them from ascending to descending  ##
+y=list(x)
+print("the number of stdents with Grade A are = ",y.count("A"))
+y.sort()
+print(y)
+
