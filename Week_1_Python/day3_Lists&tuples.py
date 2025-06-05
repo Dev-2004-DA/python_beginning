7399b8bcf6ee406ffcd20f221144511c5c59861c
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-05 17:41:45 +0530

    Lists and Tuples

---
+++ Week_1_Python/day3_Lists&tuples.py
@@ -0,0 +1,32 @@
+################## LISTS ####################
+# marks = [1,2,3,4,5]
+# print(marks[0])
+# marks[0]=0                   # change particular data in a list 
+# print(marks[0])
+# print(len(marks))            # length of the list
+
+# x=["dev",21,"22220STA018",23.5]      # list contain different data types together 
+# print(x[0]+x[2]+str(x[1]))
+# x[0]=34                              # change particular data in a list
+# print(x[0])
+# print(x)
+# print(x[1:])                         # this is list slicing 
+
+######################  LISTS METHODS #######################
+x=[2,1,3]
+x.append(4)          # add one element to the list 
+print(x)
+x.sort(reverse=True)                            # sorting in descending order
+print(x)
+x.insert(2,"dev")
+print(x)
+x.pop(3)                                        # remove the element at index 3 
+print(x)
+
+########################### TUPLES #############################
+x=(2,4,2,"dev")
+print(x[2])
+y=list(x)
+print(y)
+z=tuple(y)
+print(z)
