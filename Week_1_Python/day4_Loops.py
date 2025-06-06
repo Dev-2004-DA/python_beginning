840fa062df454130e398033c7af9d7b22f6e5807
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-06 18:11:27 +0530

    while loop

---
+++ Week_1_Python/day4_Loops.py
@@ -0,0 +1,13 @@
+############# WHILE LOOPS ##############
+i=1
+while i<=500:
+    print("HI ",i)
+    i=i+2
+print(i)    
+#### printing squares of 1st 50 natural number 
+j=1
+while(j<=50):
+    print(j*j)
+    j+=1
+print(j)    
+print("both loop ended")
