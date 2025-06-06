0f6a6e05d9c9dee2c7e8f181ef15a12c205a89fd
Dev-2004-DA <devsoni12380@gmail.com>
2025-06-06 19:06:58 +0530

    search the element x of a tupple using a loop: (1,4,9,...,100)

--- Week_1_Python/day4_practise.py
+++ Week_1_Python/day4_practise.py
@@ -29,10 +29,23 @@
 
 ######### print the element of a list using a loop: [1,4,9,...,100]############
 
-x=[1,4,9,16,25,36,49,64,81,100]
-i=0
-while(i<10):
-    print(x[i])       # it will print element at index 0 then 1 then 2 ,,,, 9
-    i+=1
-print("loop ended ")
+# x=[1,4,9,16,25,36,49,64,81,100]
+# i=0
+# while(i<10):
+#     print(x[i])       # it will print element at index 0 then 1 then 2 ,,,, 9
+#     i+=1
+# print("loop ended ")
 
+
+######### search the element x of a tupple using a loop: (1,4,9,...,100)############
+
+
+x=(1,4,9,16,25,36,49,64,81,100)
+y= 100
+i=0
+while(i<=9):
+    if(x[i]==y):         ### if statement is used to print something if item is found
+        print("",y," is at index ",i)
+    i+=1
+
+print("item is searched")
