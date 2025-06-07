7310c444ff40ae8515db877f9dc15cbc15d723e3
DEV <devsoni12380@gmail.com>
2025-06-07 18:36:48 +0530

    WAF to print the length of a list (list is the parameter )

---
+++ Week_1_Python/day5_practise.py
@@ -0,0 +1,9 @@
+####### WAF to print the length of a list (list is the parameter ) ###########
+def l(variable):
+    x=len(variable)
+    return x
+
+a="hurry"
+b=["A","B","c"]
+print("the length of",a,"is",l(a))
+print("the length of",b,"is",l(b))
