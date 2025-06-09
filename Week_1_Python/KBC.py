c390f523e8da868c4c48fd14e33c00f650f88d2f
DEV <devsoni12380@gmail.com>
2025-06-09 16:41:22 +0530

    .

--- Week_1_Python/KBC.py
+++ Week_1_Python/KBC.py
@@ -8,29 +8,29 @@ que=["number of electrons in carbon atom?".title(),"no of days in a non leap yea
 opt=["A. 4","B. 8","C. 6","D. 12","A. 368","B. 365","C. 366","D. 356","A. New delhi".title(),"B. lucknow".title(),"C. kolkata".title(),"D. mumbai".title()]
 ans=[opt[2],opt[5],opt[8]]
 
-print("hello , ",input("Your name is :"))
+print("hello , ",input("Your name is :").upper())
 print("Are you ready to begin?,(Y/N)" )
-if(input()=="Y"):                                                       #   
+if(input().upper()=="Y"):                                                       #   
     print("That's the spirit, Let's begin .")                           #        
     print("\n\n\nQ1.",que[0],"\n")                                      #q1 starts here
     for item in opt[0:4]:                                               #  used for loop to print options by using their index
         print(item)                                                     #
     C=ans[0]                                                            # assinging correct option numbeer to the correc anwer idx    
-    if(input("Enter option=")=="C"):                                    #    runnning if statemnt 
+    if(input("Enter option=").upper()=="C"):                                    #    runnning if statemnt 
         print("CORRECT".center(40))
         print("Lets come to next question")                                 
         print("\n\n\nQ2.",que[1],"\n")                                  # q2 starts here insisde a if statement
         for item in opt[4:8]:                                           #used for loo to print options by using their index    
             print(item)
         B=ans[1]                                                        # assinging correct option numbeer to the correc anwer idx 
-        if(input("Enter option=")=="B"):
+        if(input("Enter option=").upper()=="B"):
                 print("CORRECT".center(40))
                 print("Lets come to next question")
                 print("Q3.",que[2])                                      # q3 starts here           
                 for item in opt[8:12]:                                     # printing  its option using loops and options index 
                     print(item)
                     A=ans[2]                                                # assinging correct option numbeer to the correc anwer idx   
-                if(input("Enter option=")=="A"):
+                if(input("Enter option=").upper()=="A"):
                     print("CORRECT".center(40))
                     print("game is ended , thankyou for playing with us".upper())
                 else:
